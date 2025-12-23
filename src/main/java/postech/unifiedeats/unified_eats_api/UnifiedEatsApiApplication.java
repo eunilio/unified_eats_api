729498baf6210185abdaaf853ddbc9a05370020f
Felipe Eunilio Vieira dos Santos <felipe.eunilio@gmail.com>
2025-12-22 21:53:44 -0300

    Primeiro commit - projeto local

---
+++ src/main/java/postech/unifiedeats/unified_eats_api/UnifiedEatsApiApplication.java
@@ -0,0 +1,13 @@
+package postech.unifiedeats.unified_eats_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedEatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedEatsApiApplication.class, args);
+	}
+
+}
